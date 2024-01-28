8833c2d28aefb3c624f648d5b2975c5446eca460
Sukh <offcial@User.Com>
2024-01-28 10:38:23 +0530

    dat1

---
+++ android/app/src/main/kotlin/Reserva/example/reserva_user/MainActivity.kt
@@ -0,0 +1,6 @@
+package Reserva.example.reserva_user
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/logo.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class LogoSacreen extends StatelessWidget {
+  const LogoSacreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(children: [Image(image: image)],),
+    );
+  }
+}
